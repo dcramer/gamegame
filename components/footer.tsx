bf8e713491293f032c3767dffead5e0158d3a993
David Cramer <dcramer@gmail.com>
2024-09-03 14:15:33 -0700

    Better footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,16 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="container mx-auto px-4 py-8 text-center text-gray-600">
-      <p>
-        &copy; 2023 gamegame.ai &middot;{" "}
-        <Link prefetch={false} href="https://github.com/dcramer/gamegame">
+    <footer className="container mx-auto px-4 py-8 text-center text-gray-600 font-mono text-xs">
+      <p className="flex items-center justify-center gap-2">
+        <Link
+          prefetch={false}
+          href="https://github.com/dcramer/gamegame"
+          className="flex items-center gap-1 hover:underline"
+        >
+          <GitHubLogoIcon className="w-4 h-4" />
           GitHub
         </Link>
       </p>
