4c9e42ef10a3eeca8bc6d67e563b61c6226378b3
David Cramer <dcramer@gmail.com>
2024-09-05 21:49:34 -0700

    Reduce image size

--- components/chat.tsx
+++ components/chat.tsx
@@ -151,7 +151,7 @@ export function Chat({
       <div className="flex justify-between items-center mb-2 lg:mb-6">
         <div className="flex items-center">
           {game.imageUrl && (
-            <div className="w-24 h-24 relative hidden lg:block">
+            <div className="w-16 h-16 relative hidden lg:block">
               <Image
                 src={game.imageUrl}
                 alt={game.name}
