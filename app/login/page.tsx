fdadf572a9cea1b9959a1d65fca0cf25bef89912
David Cramer <dcramer@gmail.com>
2024-09-23 10:30:48 -0700

    Unused import

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,4 @@
 import { auth } from "@/auth";
-import Layout from "@/components/layout";
 import LayoutModal from "@/components/layout-modal";
 import LoginForm from "@/components/login-form";
 import { redirect } from "next/navigation";
